136cd2a4241bf0d888246ef8202a1031960c2ce5
prasannakumar <prasannakumarsinganamalla@gmail.com>
2024-05-25 21:56:56 +0530

    commited

--- provisioner.sh
+++ provisioner.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash
+" #!/usr/bin/env bash
 
 # update package
 sudo yum -y update
@@ -23,4 +23,38 @@ sudo bash install
 sudo yum install docker -y
 sudo systemctl start docker
 
-#need to install more 
+#need to install more and more "
+
+#Ubuntu
+
+#!/usr/bin/env bash
+
+# update package
+sudo apt update
+sudo apt upgrade -y
+
+# install git
+sudo apt install git -y
+
+# install SSM
+wget https://s3.amazonaws.com/ec2-downloads-windows/SSMAgent/latest/debian_amd64/amazon-ssm-agent.deb
+sudo dpkg -i amazon-ssm-agent.deb
+sudo systemctl start amazon-ssm-agent
+sudo systemctl enable amazon-ssm-agent
+
+# install cloudwatch agent
+wget https://s3.amazonaws.com/amazoncloudwatch-agent/ubuntu/amd64/latest/amazon-cloudwatch-agent.deb
+sudo dpkg -i amazon-cloudwatch-agent.deb
+sudo systemctl start amazon-cloudwatch-agent
+sudo systemctl enable amazon-cloudwatch-agent
+
+# install AWS inspector
+curl -O https://inspector-agent.amazonaws.com/linux/latest/install
+sudo bash install
+
+# install docker 
+sudo apt install docker.io -y
+sudo systemctl start docker
+
+#need to install more and more
+
