fc3c95bdf91fe37aba14b44c7f8201cc72d63d0d
prasannakumar <prasannakumarsinganamalla@gmail.com>
2024-09-22 22:26:02 +0530

    updated the file

--- provisioner.sh
+++ provisioner.sh
@@ -1,57 +1,28 @@
-" #!/usr/bin/env bash
-
-# update package
-sudo yum -y update
-
-# install git
-sudo yum install git -y
-
-# install SSM
-sudo yum install https://s3.amazonaws.com/ec2-downloads-windows/SSMAgent/latest/linux_amd64/amazon-ssm-agent.rpm -y
-#sudo systemctl start amazon-ssm-agent
-
-# install cloudwatch agent
-sudo yum install https://s3.amazonaws.com/amazoncloudwatch-agent/amazon_linux/amd64/latest/amazon-cloudwatch-agent.rpm -y
-sudo /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl -m ec2 -a start
-sudo /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl -m ec2 -a status
-
-# install AWS inspector
-curl -O https://inspector-agent.amazonaws.com/linux/latest/install
-sudo bash install
-
-#install docker 
-sudo yum install docker -y
-sudo systemctl start docker
-#-----------------------------------------------------------------------------
-#Ubuntu
-
 #!/usr/bin/env bash
 
-# update package
-sudo apt update
-sudo apt upgrade -y
+# Update package list
+sudo apt update && sudo apt upgrade -y
 
-# install git
+# Install git
 sudo apt install git -y
 
-# install SSM
+# Install SSM
 wget https://s3.amazonaws.com/ec2-downloads-windows/SSMAgent/latest/debian_amd64/amazon-ssm-agent.deb
 sudo dpkg -i amazon-ssm-agent.deb
 sudo systemctl start amazon-ssm-agent
 sudo systemctl enable amazon-ssm-agent
 
-# install cloudwatch agent
+# Install CloudWatch Agent
 wget https://s3.amazonaws.com/amazoncloudwatch-agent/ubuntu/amd64/latest/amazon-cloudwatch-agent.deb
 sudo dpkg -i amazon-cloudwatch-agent.deb
 sudo systemctl start amazon-cloudwatch-agent
 sudo systemctl enable amazon-cloudwatch-agent
 
-# install AWS inspector
+# Install AWS Inspector
 curl -O https://inspector-agent.amazonaws.com/linux/latest/install
 sudo bash install
 
-# install docker 
+# Install Docker
 sudo apt install docker.io -y
 sudo systemctl start docker
-usermode -aG docker $USER
-
+sudo usermod -aG docker $USER
